f41d353f5cf4001afcc2e43851bcc20fe95aa63b
neilpatelll <neil0patel@gmail.com>
2024-06-10 16:46:50 -0500

    Add files via upload

---
+++ spoof/spoof/spoofApp.swift
@@ -0,0 +1,17 @@
+//
+//  spoofApp.swift
+//  spoof
+//
+//  Created by Neil Patel on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct spoofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
